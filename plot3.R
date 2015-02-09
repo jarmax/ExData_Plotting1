4b3b73acaf3e3af86615f245ebb71e6d35cb9357
Juan A. Ramirez-Macias <juan.ramirez@upb.edu.co>
2015-02-08 20:19:22 -0500

    Homework third commit

--- plot3.R
+++ plot3.R
@@ -4,14 +4,20 @@ data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",
 
 # Subset file - needs improvement
 dataSubset <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
-gap <- dataSubset$Global_active_power
+
+sm1 <- dataSubset$Sub_metering_1
+sm2 <- dataSubset$Sub_metering_2
+sm3 <- dataSubset$Sub_metering_3
 time <- strptime(paste(dataSubset$Date,dataSubset$Time),
                  format = "%d/%m/%Y %H:%M:%S")
 
 # Open png device
-png(filename = "plot2.png",
-    width = 480, height = 480, units = "px")
+# png(filename = "plot3.png",
+#     width = 480, height = 480, units = "px")
 # Create plot
-plot(time, gap, main = "", xlab = "", ylab = "Global active power (kW)", type = "l")
+plot(time, sm1, main = "", xlab = "", ylab = "Energy sub metering",
+     type = "l", col = "green")
+lines(time, sm2, col = "red")
+lines(time, sm3)
 # Turn off device
 dev.off()
